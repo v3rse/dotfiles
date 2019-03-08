58808280e3b86a1ae816c588db5203a014a96bac
Nana Adane <nanaofosuheneadane@gmail.com>
2019-03-08 21:22:37 +0000

    New wallpaper and settings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,6 +15,20 @@ alias work="cd ~/TheFactory/code/work/kudobuzz"
 alias learn="cd ~/TheFactory/code/learn"
 alias oss="cd ~/TheFactory/code/oss"
 
+# -Xrandr
+alias screen-left='xrandr --output eDP-1 --output DP-1 --auto --left-of eDP-1'
+alias screen-right='xrandr --output eDP-1 --output DP-1 --auto --right-of eDP-1'
+alias screen-off='xrandr --output DP-1 --auto --off'
+alias sl='screen-left'
+alias sr='screen-right'
+alias sx='screen-off'
+
+# -i3 workspace
+alias workspace-left='i3-msg move workspace to output left'
+alias workspace-right='i3-msg move workspace to output right'
+alias wl='workspace-left'
+alias wr='workspace-right'
+
 # -Python
 #alias python="python3"
 #alias pip="pip3"
@@ -40,6 +54,15 @@ alias fuzzy='fzf --preview="head -$LINES {}"'
 alias love='/Applications/love.app/Contents/MacOS/love'
 
 alias journal='cd ~/TheFactory/docs; code journal; cd -'
+
+# -Git
+alias gs='git status '
+alias ga='git add '
+alias gaa='git add -A '
+alias gb='git branch '
+alias gc='git commit '
+alias gcm='git commit -m '
+alias go='git checkout '
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/v3rse/Downloads/google-cloud-sdk/path.fish.inc' ]
