0fcd6561d02a6d9ba0d4bbe8e3d25b6856bdf17c
nanaadane <nadane@botkeeper.com>
2022-01-03 23:34:42 +0000

    remove some aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,9 +7,6 @@ alias projects="cd ~/projects"
 # -NPM
 alias nr='npm run'
 
-# -Fuzzy Find
-alias fuzzy='fzf --preview="head -$LINES {}"'
-
 # -Git
 alias gs='git status '
 alias ga='git add '
@@ -18,6 +15,3 @@ alias gb='git branch '
 alias gc='git commit '
 alias gcm='git commit -m '
 alias gco='git checkout '
-
-# Utils
-alias vim='nvim'
