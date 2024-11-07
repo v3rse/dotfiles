9ef30947e6419a368c0dd2340dc6978c1bdc2424
Nana Adane <nanaofosuheneadane@gmail.com>
2024-11-07 22:24:56 +0000

    add other emacs configurations

---
+++ emacs/.emacs-profiles.el
@@ -0,0 +1,3 @@
+(("vanilla" . ((user-emacs-directory . "~/.emacs.default")))
+ ("default" . ((user-emacs-directory . "~/.config/emacs-configs/doom-emacs")
+           (env . (("DOOMDIR" . "~/.config/doom"))))))
