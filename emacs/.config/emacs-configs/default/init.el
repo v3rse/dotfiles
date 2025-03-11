912c1e41198d4bca167a875a4b8b0c5cfc653924
Nana Adane <nanaofosuheneadane@gmail.com>
2025-03-11 04:08:25 +0100

    new init file moving way from vanilla

--- emacs/.config/emacs-configs/default/init.el
+++ emacs/.config/emacs-configs/default/init.el
@@ -1,82 +1,85 @@
-(setq gc-cons-threshold (* 100 1024 1024))
-(setq read-process-output-max (* 1024 1024)) ;; 1MB
-(setq large-file-warning-threshold (* 100 1024 1024)) ;; 100MB
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-(setq make-backup-files nil)
+;; general
+(use-package emacs
+  :ensure nil
 
-(setq create-lockfiles nil)
+  :config
+  ;; font selection
+  (let ((mono-spaced-font "Iosevka")
+        (proportionately-spaced-font "Iosevka Etoile"))
 
-(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
-(setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
+    (set-face-attribute 'default nil :family mono-spaced-font :height 120)
+    (when (eq system-type 'darwin)
+      (set-face-attribute 'default nil :family mono-spaced-font :height 140))
+  
+    (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
+    (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.0))
+
+  ;; recent files list size
+  (setq recentf-max-menu-items 25)
+  
+  ;;diary
+  (setq diary-file "~/org/emacs-diary")
+  
+  ;; locks, autosave and backup
+  (setq make-backup-files nil)
+  (setq create-lockfiles nil)
+  (make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
+  (setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
       auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
 
-;; Keybindings
-(global-set-key (kbd "C-x C-b") 'ibuffer)
+  ;; custom files location
+  (setq custom-file (locate-user-emacs-file "custom.el"))
+  (load custom-file 'noerror 'nomessage)
 
-;; Better Defaults
-(setq visible-bell t
-      custom-file (expand-file-name "custom.el" user-emacs-directory))
+  (setq-default fill-column 90)
 
-;; Basic UI configuration
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-(setq inhibit-startup-screen t)
-(setq initial-scratch-message "")
-(set-face-attribute 'default nil :family "Iosevka" :height 125  :weight 'medium :slant 'normal :width 'normal)
-(set-face-attribute 'variable-pitch nil :family "Iosevka Etoile" :height 130)
+  
+  :init
+  ;; gui bars
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (when scroll-bar-mode
+    (scroll-bar-mode -1))
+  (indent-tabs-mode -1)
 
-(tooltip-mode 0)
-(global-completion-preview-mode 1)
-(which-key-mode 1)
-;;(tab-bar-mode 0)
-;;(desktop-save-mode 1)
+  ;; files and buffers
+  (global-hl-line-mode 1)
+  (global-auto-revert-mode 1)
+  (global-completion-preview-mode 1)
+  
+  ;; match bracket pairs
+  (electric-pair-mode 1)
+  ;; auto indent
+  (electric-indent-mode 1)
 
-;; tab bar mode
-;;(setq tab-bar-show nil)
-;;(add-hook 'emacs-startup-hook #'tab-switcher)
+  ;; history and completions
+  (savehist-mode 1)
+  (recentf-mode 1)
+  (fido-vertical-mode 1) ;; replaces vertico
 
-(setq completion-styles '(basic flex)
-      completion-auto-select t
-      completion-auto-help 'visible
-      completions-format 'one-column
-      completions-sort 'historical
-      completions-max-height 20
-      completion-ignore-case t)
+  ;; help
+  (which-key-mode 1)
 
-(fido-vertical-mode 1)
+  :bind (("C-x C-r" . recentf-open-files)
+	 ("C-x C-b" . ibuffer))
+)
 
+;; usability
+(use-package eww
+  :config
+  (setq browse-url-browser-function 'eww-browse-url))
 
-;; Simple fringe configuration
-(setq default-frame-alist '(
-			    (left-fringe . 0) (right-fringe . 0)
-			    (internal-border-width . 24)
-			    (bottom-divider-width . 0) (right-divider-bars . 0)
-			    (undecorated-round . t)))
-(modify-frame-parameters nil default-frame-alist)
+(use-package delsel
+  :ensure nil
+  :hook (after-init . delete-selection-mode))
 
-;; recent files
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(global-set-key (kbd "C-x C-r") 'recentf-open-files)
-
-;; save place
-(save-place-mode 1)
-
-;; follow symlinks under version control(the linked file is not tracked)
-(setq vc-follow-symlinks t)
-
-;; enable clipbard
-(setq select-enable-clipboard t)
-
-;; match bracket pairs
-(electric-pair-mode 1)
-
-;; auto indent
-(electric-indent-mode 1)
-
-;; Basic Coding Settings
 (use-package eglot
+  :ensure nil
+  :config
+  (setq eglot-autoshutdown t)
   :hook ((python-mode . eglot-ensure)
          (js-mode . eglot-ensure)
          (typescript-mode . eglot-ensure)
@@ -84,39 +87,40 @@
          (html-mode . eglot-ensure)
          (css-mode . eglot-ensure)))
 
-(use-package treesit
+
+(use-package window
+  :ensure nil
+  :custom
+  (display-buffer-alist
+   '(
+      ;; auxiliary buffers: bottom 25%
+      ("\\*\\(Backtrace\\|Warnings\\|Compile-Log\\|[Hh]elp\\|Messages\\|Bookmark List\\|Ibuffer\\|Occur\\|eldoc.*\\)\\*"
+      (display-buffer-in-side-window)
+      (window-height . 0.25)
+      (side . bottom)
+      (slot . 0))
+
+      ;; flymake and completions: bottom 25%
+      ("\\*\\(Flymake diagnostics\\|xref\\|ivy\\|Swiper\\|Completions\\)"
+      (display-buffer-in-side-window)
+      (window-height . 0.25)
+      (side . bottom)
+      (slot . 1))
+      )))
+
+(use-package dired
+  :ensure nil
+  :commands (dired)
+  :hook
+  ((dired-mode . dired-hide-details-mode)
+   (dired-mode . hl-line-mode))
   :config
-  (setq major-mode-remap-alist
-        '((c-mode          . c-ts-mode)
-          (c++-mode        . c++-ts-mode)
-          (css-mode        . css-ts-mode)
-          (java-mode       . java-ts-mode)
-          (javascript-mode . js-ts-mode)
-          (js-json-mode    . json-ts-mode)
-          (python-mode     . python-ts-mode)
-          (typescript-mode . typescript-ts-mode)
-          (ruby-mode       . ruby-ts-mode)
-          (go-mode         . go-ts-mode)
-          (rust-mode       . rust-ts-mode))))
+  (setq dired-recursive-copies 'always)
+  (setq dired-recursive-deletes 'always)
+  (setq delete-by-moving-to-trash t)
+  (setq dired-dwim-target t))
 
-(setq eglot-autoshutdown t)
 
-(use-package project
-  :bind (("C-x p f" . project-find-file)
-	 ("C-x p p" . project-switch-project)
-	 ("C-x p s" . project-search)
-	 ("C-x p k" . project-kill-buffers)
-	 ("C-x p t" . project-shell)
-	 ("C-x p d" . project-dired)))
-
-(use-package xref
-  :bind (("M-?" . xref-find-references)
-         ("M-." . xref-find-definitions)
-         ("M-," . xref-go-back)))
-;; Diary
-(setq diary-file "~/org/emacs-diary")
-
-;; Org
 (use-package org
   :config (setq org-directory "~/org/"
                 org-default-notes-file "~/org/inbox.org"
@@ -132,29 +136,11 @@
 		org-refile-use-outline-path t
 		org-outline-path-complete-in-steps nil
 		org-M-RET-may-split-line '((default . nil))
-		org-insert-heading-respect-content t))
-
-(setq-default fill-column 90)
-(add-hook 'org-mode-hook 'visual-line-mode)
-
-(use-package org-attach
-  :after org
-  :ensure nil)
-
-(use-package org-protocol
-  :after org
-  :ensure nil)
-
-(use-package org-tempo
-  :after org
-  :ensure nil
-  :config
-  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-  (add-to-list 'org-structure-template-alist '("q" . "quote")))
-
-(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "HOLD(h)" "PROJ(p)" "|" "DONE(d)" "CNCL(c)")))
-
-(setq org-capture-templates
+		org-insert-heading-respect-content t)
+  ;; keywords
+  (setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "HOLD(h)" "PROJ(p)" "|" "DONE(d)" "CNCL(c)")))
+  ;; capture templates
+  (setq org-capture-templates
         `(("t" "Task" entry (file+headline "inbox.org" "Tasks")
            ,(string-join '("* TODO %?"
                            ":PROPERTIES:"
@@ -191,10 +177,8 @@
 	  ("j" "Journal" entry (file+datetree "journal.org")
 	   "* %?\nEntered on %U\n %i\n %a")
 	  ))
-	
-(add-to-list 'org-modules 'org-habit)
-
- (setq org-agenda-custom-commands
+  ;; agenda
+  (setq org-agenda-custom-commands
         '(("g" "Get Things Done (GTD)"
                 ((agenda ""
                          ((org-agenda-span 'day)
@@ -223,28 +207,54 @@
                  (tags "CLOSED>=\"<today>\""
                        ((org-agenda-overriding-header "\nCompleted today\n")))
                  ))))
+ ;; module
+ (add-to-list 'org-modules 'org-habit)
 
-(setq v3rse/org-refile-target-files '("agenda.org"
+ ;; refile
+ (setq v3rse/org-refile-target-files '("agenda.org"
                                       "projects.org"
                                       "someday-maybe.org"
                                       "notes.org"))
 
-
-(setq v3rse/org-refile-file-paths
+ (setq v3rse/org-refile-file-paths
       (let (result)
         (dolist (file v3rse/org-refile-target-files result)
           (push (expand-file-name file org-directory) result))))
 
-(setq org-refile-targets
+ (setq org-refile-targets
       '((nil :maxlevel . 9)
         (v3rse/org-refile-file-paths :maxlevel . 9)))
 
-(define-key global-map (kbd "C-c a") 'org-agenda)
-(define-key global-map (kbd "C-c c") 'org-capture)
-(define-key global-map (kbd "C-c l") 'org-store-link)
+ :hook ((org-mode-hook . visual-line-mode))
+ :bind (("C-c a" . org-agenda)
+	("C-c c" . org-capture)
+	("C-c l" . org-store-link)))
 
-;; News
-(setq newsticker-url-list
+(use-package org-attach
+  :after org
+  :ensure nil)
+
+(use-package org-protocol
+  :after org
+  :ensure nil)
+
+(use-package org-tempo
+  :after org
+  :ensure nil
+  :config
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("q" . "quote")))
+
+(use-package newst-reader
+  :custom
+  (custom-set-faces
+   '(newsticker-feed-face ((t (:inherit ef-themes-heading-1 :height 1.4))))
+   '(newsticker-new-item-face ((t (:inherit ef-themes-heading-4 :height 1.2)))))
+  :hook ((newsticker-mode-hook . variable-pitch-mode))
+  :config
+  (setq newsticker-frontend 'newsticket-plainview)
+  (setq newsticker-use-full-width nil)
+  (setq newsticker-url-list
       '(
           ("Euronews" "https://www.euronews.com/rss")
           ("Allsides News" "https://www.allsides.com/rss/news")
@@ -259,48 +269,170 @@
 ;;	  ("Org Mode Woof Feed" "https://tracker.orgmode.org/index.rss")
 ;;	  ("Joshua Blais" "https://joshblais.com/index.xml")
 	)
-      )
+     )
+  )
 
-;; Theming
-(use-package emacs
+;; -- external --
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :bind
+  ( :map dired-mode-map
+    ("<tab>" . dired-subtree-toggle)
+    ("TAB" . dired-subtree-toggle)
+    ("<backtab>" . dired-subtree-remove)
+    ("S-TAB" . dired-subtree-remove))
   :config
-  (require-theme 'modus-themes)
+  (setq dired-subtree-use-backgrounds nil))
+
+(use-package trashed
+  :ensure t
+  :commands (trashed)
+  :config
+  (setq trashed-action-confirmer 'y-or-n-p)
+  (setq trashed-use-header-line t)
+  (setq trashed-sort-key '("Date deleted" . t))
+  (setq trashed-date-format "%Y-%m-%d %H:%M:%S"))
+
+(use-package treesit-auto
+  :ensure t
+  :after emacs
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode t))
+
+(use-package marginalia
+  :ensure t
+  :hook (after-init . marginalia-mode))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil)
+  (setq completion-category-overrides nil))
+
+(use-package corfu
+  :ensure t
+  :hook (after-init . global-corfu-mode)
+  :bind (:map corfu-map ("<tab>" . corfu-complete))
+  :config
+  (setq tab-always-indent 'complete)
+  (setq corfu-preview-current nil)
+  (setq corfu-min-width 20)
+
+  (setq corfu-popupinfo-delay '(1.25 . 0.5))
+  (corfu-popupinfo-mode 1) ; shows documentation after `corfu-popupinfo-delay'
+
+  ;; Sort by input history (no need to modify `corfu-sort-function').
+  (with-eval-after-load 'savehist
+    (corfu-history-mode 1)
+    (add-to-list 'savehist-additional-variables 'corfu-history)))
+
+(use-package ef-themes
+  :ensure t
+  :config
+  (load-theme 'ef-dream))
+
+(use-package nerd-icons
+  :ensure t
+  :defer t) ;; only load when needed
+
+(use-package nerd-icons-completion
+  :ensure t
+  :defer t
+  :after marginalia
+  :config
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+
+(use-package nerd-icons-corfu
+  :ensure t
+  :defer t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+(use-package nerd-icons-dired
+  :ensure t
+  :defer t
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+(use-package doom-modeline
+  :ensure t
+  :defer t
+  :custom
+  (doom-modeline-buffer-file-name-style 'buffer-name)  
+  (doom-modeline-project-detection 'project)           
+  (doom-modeline-buffer-name t)                        
+  (doom-modeline-vcs-max-length 25)                    
+  :config
+  (setq doom-modeline-icon t)                        
+  :hook
+  (after-init . doom-modeline-mode))
+
+(use-package spacious-padding
+  :ensure t
+  :if (display-graphic-p)
+  :hook (after-init . spacious-padding-mode)
+  :bind ("<f8>" . spacious-padding-mode)
+  :config
+  ;; These are the defaults, but I keep it here for visiibility.
+  (setq spacious-padding-widths
+        '( :internal-border-width 30
+           :header-line-width 4
+           :mode-line-width 6
+           :tab-width 4
+           :right-divider-width 30
+           :scroll-bar-width 8
+           :left-fringe-width 20
+           :right-fringe-width 20))
+
+  ;; Read the doc string of `spacious-padding-subtle-mode-line' as
+  ;; it is very flexible.
+  (setq spacious-padding-subtle-mode-line
+        '( :mode-line-active spacious-padding-subtle-mode-line
+           :mode-line-inactive spacious-padding-subtle-mode-line-inactive))
+  :init
+  (spacious-padding-mode 1))
+
+(use-package keycast
+  :ensure t
+  :init
+  (keycast-header-line-mode 1))
+
+;; -- EXWM
+;; (use-package exwm
+;;   :ensure t
+;;   :config
+;;   (setq exwm-workspace-number 4)
+;;   ;; application class name = buffer name
+;;   (add-hook 'exwm-update-class-hook
+;; 	    (lambda () (exwm-workspace-rename-buffer exwm-class-name)))
+;;   ;; keybindings
+;;   (setq exwm-input-global-keys
+;; 	`(([?\s-r] . exwm-reset) ;; s-r: Reset (to line-mode)
+;; 	  ([?\s-w] . exwm-workspace-switch) ;; s-w: Switch workspace.
+;; 	  ([?\s-&] . (lambda (cmd) ;; s-&: Launch application
+;; 		       (interactive (list (read-shell-command "$ ")))
+;;  		       (start-process-shell-command cmd nil cmd)))
+;; 	  ;; s-No. : Switch to workspace by number
+;; 	  ,@(mapcar (lambda (i)
+;; 		      `(,(kbd (format "s-%d" i)) .
+;; 			(lambda ()
+;; 			  (interactive)
+;; 			  (exwm-workspace-switch-create ,i))))
+;; 		    (number-sequence 0 9))))
+;;   :init
+;;   (use-package exwm-systemtray
+;;    :config
+;;    (setq exwm-systemtray-height 32) 
+;;    :init
+;;    (exwm-systemtray-mode 1))
   
-  (setq modus-themes-italic-constructs t
-      modus-themes-bold-constructs nil
-      modus-themes-mixed-fonts t
-      modus-themes-custom-auto-reload nil
-      modus-themes-prompts '(italic bold)
-      modus-themes-custom-auto-reload t
-      modus-themes-disable-other-themes t
-      modus-themes-variable-pitch-ui nil
-      modus-themes-completions
-      '((matches . (extrabold))
-	(selection . (semibold italic text-also)))
-      modus-themes-org-blocks 'gray-background
-      modus-themes-headings
-      '((0 . (variable-pitch 1.8))
-	(1 . (variable-pitch 1.5))
-	(2 . (1.3))
-	(agenda-date . (1.3))
-	(agenda-structure . (variable-pitch light 1.8))
-	(t . (1.1)))
-      modus-themes-common-palette-overrides
-      `(
-	;; (bg-main "#2E3440") (fg-main "#ECEFF4")
-	;; (cursor "#dfaf7a")
-	(border-mode-line-active unspecified)
-	(border-mode-line-inactive unspecified)
-	,@modus-themes-preset-overrides-cooler))
-  
-  ;; Load a simple theme
-  (load-theme 'modus-vivendi-tinted :no-confirm)
-  (define-key global-map (kbd "<f5>") #'modus-themes-toggle))
+;;   (exwm-enable))
 
-
-;; Default Brower
-(setq browse-url-browser-function 'eww-browse-url)
-
-;; Server
-(server-start)
-
+;; -- Server
+;; (server-start)
