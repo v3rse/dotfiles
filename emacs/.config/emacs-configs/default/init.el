f8485a73ff023fab055289f18e3037cb99a85099
Nana Adane <nanaofosuheneadane@gmail.com>
2025-07-01 01:41:01 +0200

    Add extra (project based) functionality to vterm

--- emacs/.config/emacs-configs/default/init.el
+++ emacs/.config/emacs-configs/default/init.el
@@ -637,9 +637,50 @@
   (setq kubernetes-poll-frequency 3600
         kubernetes-redraw-frequency 3600))
 
+(defun v3rse/ansi-color-compilation-buffer-hook ()
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region compilation-filter-start (point))))
+
+(use-package ansi-color
+  :hook (compilation-filter . v3rse/ansi-color-compilation-buffer-hook))
+
 (use-package vterm
   :ensure t
+  :hook
+  (vterm-mode . compilation-shell-minor-mode)
   :bind (("C-c t" . vterm)))
+
+(use-package multi-vterm
+  :ensure t
+  :when (and (not (featurep 'os/win)) (featurep 'feat/modules))
+  :bind (([remap project-shell] . multi-vterm-project)
+         ([f1] . +multi-vterm-toggle-dwim)
+         :map vterm-mode-map ([f1] . +multi-vterm-toggle-dwim))
+  :custom
+  (multi-vterm-dedicated-window-height-percent 20)
+  :config
+  ;; If a dedicated terminal is run on a remote machine, it seems that
+  ;; `multi-vterm' don't get the working directory right, lets fix it!
+  (advice-add
+   'multi-vterm-dedicated-open :after
+   (satch-defun +multi-vterm--remote-change-working-directory:after-a (&rest _)
+     (when-let* ((dir (file-remote-p default-directory 'localname)))
+       (vterm-send-string (format "cd " (shell-quote-argument dir)))
+       (vterm-send-return))))
+
+  (defun +multi-vterm-toggle-dwim ()
+    "Toggle the vterm window.
+When in a project, toggle a `multi-vterm-project' terminal. When outside
+a project, call `multi-vterm-dedicated-toggle'."
+    (interactive)
+    (if-let* ((buf-name (and (multi-vterm-project-root) (multi-vterm-project-get-buffer-name))))
+        (if-let* ((buf (get-buffer buf-name))
+                  ((buffer-live-p buf)))
+            (if-let* ((win (get-buffer-window buf))) ; The project's vterm already exists, toggle it's window
+                (delete-window win)
+              (pop-to-buffer buf))
+          (multi-vterm-project))
+      (multi-vterm-dedicated-toggle))))
 
 (use-package ace-window
   :ensure t
