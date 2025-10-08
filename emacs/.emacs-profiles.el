1807a5d60d4c3bca3c5ff698f13708cb6fdafca0
Nana Ofosuhene Adane <nanaofosuheneadane@gmail.com>
2025-10-08 12:14:20 +0200

    New Doom font and emacs kick profile

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,3 +1,4 @@
 (("doom" . ((user-emacs-directory . "~/.config/emacs-configs/doomemacs")
             (env . (("DOOMDIR" . "~/.config/doom")))))
+ ("kick" . ((user-emacs-directory . "~/.config/emacs-configs/emacskick")))
  ("default" . ((user-emacs-directory . "~/.config/emacs-configs/default"))))
